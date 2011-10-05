35ab0dc033cf67d6e70573fbc2ef5a49afd6eaca
Orion Jankowski <orion10@gmail.com>
2011-10-05 15:23:55 -0700

    Expanded the test just slightly to demonstrate multiple output formats.

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,21 @@
 var Arkle = require('../arkleseizure').Arkleseizure;
 var a = new Arkle();
+var b = new Arkle();
 
 a.SetInFormat('smi');
-a.SetOutFormat('mol');
+a.SetOutFormat('can');
+
+b.SetInFormat('smi');
+b.SetOutFormat('mol');
+
 console.log(a.Convert("CCC"));
+console.log(b.Convert("CCC"));
+
+console.log(a.Convert("CCC=C"));
+console.log(b.Convert("CCC=C"));
+
+console.log(a.Convert("CCC(CCC)(CC)C"));
+console.log(b.Convert("CCC(CCC)(CC)C"));
 
 console.log('Arkleseizure sneezed!');
 
